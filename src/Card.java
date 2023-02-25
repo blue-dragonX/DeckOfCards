7547934bc16c8792b15068a509f0c4bd362a9859
blue-dragonX <113529065+blue-dragonX@users.noreply.github.com>
2023-02-24 23:37:32 -0500

    DSKTP 2/24/2023 23:37

--- src/Card.java
+++ src/Card.java
@@ -1,13 +1,26 @@
 public class Card {
     private int cardNumber;
+    private char cardStatus; //J,Q,K,A if needed
     private String cardSuit;
 
     public Card(int cardNumber,String cardSuit){
         this.cardNumber=cardNumber;
         this.cardSuit=cardSuit;
+        switch(cardNumber){
+            case 11-> cardStatus = 'J';
+            case 12-> cardStatus = 'Q';
+            case 13-> cardStatus = 'K';
+            case 14-> cardStatus = 'A';
+            default->{}
+        }
+
     }
 
     public void print() {
+        if (cardNumber<=10)
         System.out.printf("%d%s\n",cardNumber,cardSuit);
+       else
+           System.out.printf("%c%s\n", cardStatus, cardSuit);
     }
+
 }
